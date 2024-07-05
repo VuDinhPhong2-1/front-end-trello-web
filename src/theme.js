fa029a8305d2faf4e4c92210c3032fc4de19a3df
VuDinhPhong <vudinhphong.261001@gmail.com>
2024-07-05 14:12:46 +0700

    refactor_theme_color

--- src/theme.js
+++ src/theme.js
@@ -1,26 +1,26 @@
 import { experimental_extendTheme as extendTheme } from "@mui/material/styles"
-import { cyan, deepOrange, orange, teal } from "@mui/material/colors"
+// import { cyan, deepOrange, orange, teal } from "@mui/material/colors"
 
 const theme = extendTheme({
   trello:{
     appBarHeight: "48px",
     boardBarHeight: "58px"
   },
-  colorSchemes: {
-    light: {
-      palette: {
-        primary: teal,
-        secondary: deepOrange
-      }
-    },
-    dark: {
-      palette: {
-        primary: cyan,
-        secondary: orange
-      },
-      spacing: (factor) => `${0.25 * factor}rem`
-    }
-  },
+  // colorSchemes: {
+  //   light: {
+  //     palette: {
+  //       primary: teal,
+  //       secondary: deepOrange
+  //     }
+  //   },
+  //   dark: {
+  //     palette: {
+  //       primary: cyan,
+  //       secondary: orange
+  //     },
+  //     spacing: (factor) => `${0.25 * factor}rem`
+  //   }
+  // },
   components:{
     MuiCssBaseline:{
       styleOverrides:{
@@ -30,11 +30,11 @@ const theme = extendTheme({
             height: "8px"
           },
           "*::-webkit-scrollbar-thumb":{
-            background:"#b2bec3",
+            background:"#dcdde1",
             borderRadius: "8px"
           },
           "*::-webkit-scrollbar-thumb:hover":{
-            background:"#55efc4"
+            background:"white"
           }
         }
       }
@@ -42,31 +42,27 @@ const theme = extendTheme({
     MuiButton: {
       styleOverrides:{
         root:{
-          textTransform:"none"
+          textTransform:"none",
+          borderWidth: "0.5px",
+          "&:hover":{
+            borderWidth: "0.5px",
+          }
         }
       }
     },
-    MuiOutlinedInput:{
-      styleOverrides:{
-        root:({ theme }) => ({
-          color: theme.palette.primary.main,
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
           fontSize: "0.875rem",
-          ".MuiOutlinedInput-notchedOutline": { borderColor: theme.palette.primary.light },
-          "&:hover":{
-            ".MuiOutlinedInput-notchedOutline": { borderColor: theme.palette.primary.main },
-          },
-          "& fieldset":{
-            borderWidth: "1px !important"
-          }
-        })
+          "& fieldset": { borderWidth: "0.5px !important" },
+          "&:hover fieldset": { borderWidth: "2px !important" },
+          "&.Mui-focused fieldset": { borderWidth: "2px !important" },
+        }
       }
     },
     MuiInputLabel: {
       styleOverrides:{
-        root: ({ theme }) => ({
-          color: theme.palette.primary.main,
-          fontSize: "0.87rem"
-        })
+        root: { fontSize: "0.87rem" }
       }
     },
   }
