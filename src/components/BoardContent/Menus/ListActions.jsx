e6ad7d9b93fe6c961bb403fd407189d7926042e4
VuDinhPhong <vudinhphong.261001@gmail.com>
2024-07-08 10:37:17 +0700

    trello board content

---
+++ src/components/BoardContent/Menus/ListActions.jsx
@@ -0,0 +1,95 @@
+import * as React from "react"
+import IconButton from "@mui/material/IconButton"
+import Menu from "@mui/material/Menu"
+import MenuItem from "@mui/material/MenuItem"
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz"
+import { Divider, ListItemIcon, Tooltip } from "@mui/material"
+import { Logout } from "@mui/icons-material"
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever"
+import ContentCutIcon from "@mui/icons-material/ContentCut"
+import ContentCopyIcon from "@mui/icons-material/ContentCopy"
+import ContentPasteIcon from "@mui/icons-material/ContentPaste"
+import AddCardIcon from "@mui/icons-material/AddCard"
+
+const ITEM_HEIGHT = 48
+
+export default function LongMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  return (
+    <div>
+      <Tooltip title="More Options">
+        <IconButton
+          aria-label="more"
+          id="basic-column-dropdown"
+          aria-controls={open ? "basic-menu-column-dropdown" : undefined}
+          aria-expanded={open ? "true" : undefined}
+          aria-haspopup="true"
+          onClick={handleClick}
+        >
+          <MoreHorizIcon />
+        </IconButton>
+      </Tooltip>
+      <Menu
+        id="basic-menu-column-dropdown"
+        MenuListProps={{
+          "aria-labelledby": "long-button",
+        }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        PaperProps={{
+          style: {
+            maxHeight: ITEM_HEIGHT * 4.5,
+            width: "230px",
+          },
+        }}
+      >
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <AddCardIcon fontSize="small" />
+          </ListItemIcon>
+          Add New Card
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <ContentCutIcon fontSize="small" />
+          </ListItemIcon>
+          Cut
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <ContentCopyIcon fontSize="small" />
+          </ListItemIcon>
+          Copy
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <ContentPasteIcon fontSize="small" />
+          </ListItemIcon>
+          Past
+        </MenuItem>
+        <Divider />
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <DeleteForeverIcon fontSize="small" />
+          </ListItemIcon>
+          Remove This Column
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Archive This Column
+        </MenuItem>
+      </Menu>
+    </div>
+  )
+}
--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -1,4 +1,11 @@
-import { Box } from "@mui/material"
+import { Box, Button, Tooltip, Typography } from "@mui/material"
+import LongMenu from "~/components/BoardContent/Menus/ListActions"
+import AddCardIcon from "@mui/icons-material/AddCard"
+import DragHandleIcon from "@mui/icons-material/DragHandle"
+import ItemCard from "~/components/BoardContent/Card/Card"
+
+const COLUMN_HEADER_HEIGHT = "50px"
+const COLUMN_FOOTER_HEIGHT = "56px"
 
 function BoardContent() {
   return (
@@ -7,13 +14,166 @@ function BoardContent() {
         bgcolor: (theme) =>
           theme.palette.mode === "dark" ? "#34495e" : "#1976d2",
         width: "100%",
-        display: "flex",
-        alignItems: "center",
-        height: (theme) =>
-          `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`,
+        height: (theme) => theme.trello.boardContentHeight,
+        p: "10px 0",
       }}
     >
-      Board content
+      <Box // List column
+        sx={{
+          bgcolor: "inherit",
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          overflowX: "auto",
+          overflowY: "hiden",
+          "&::-webkit-scrollbar-track": { m: 2 },
+        }}
+      >
+        {/* Box column */}
+        <Box
+          sx={{
+            minWidth: "300px",
+            maxWidth: "300px",
+            bgcolor: (theme) =>
+              theme.palette.mode === "dark" ? "#333643" : "#ebecf0",
+            ml: 2,
+            borderRadius: "6px",
+            height: "fit-cotent",
+            maxHeight: (theme) =>
+              `calc(${theme.trello.boardContentHeight} - ${theme.spacing(5)})`,
+          }}
+        >
+          {/* Box column header*/}
+          <Box
+            sx={{
+              height: COLUMN_HEADER_HEIGHT,
+              p: 2,
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Typography
+              variant="h6"
+              sx={{ fontWeight: "bold", cursor: "pointer", fontSize: "1rem" }}
+            >
+              Column Tilte
+            </Typography>
+            <LongMenu />
+          </Box>
+          {/* Box list card*/}
+          <Box
+            sx={{
+              p: "0 5px",
+              m: "0 5px",
+              display: "flex",
+              flexDirection: "column",
+              // p: 2,
+              gap: 2,
+              overflowX: "hine",
+              overflowY: "auto",
+              maxHeight: (theme) => `calc(${theme.trello.boardContentHeight} -
+            ${theme.spacing(5)} -
+            ${COLUMN_HEADER_HEIGHT} -
+            ${COLUMN_FOOTER_HEIGHT}
+            )`,
+              "&::-webkit-scrollbar-thumb": {
+                background: "#ced0da",
+                borderRadius: "8px",
+              },
+              "&::-webkit-scrollbar-thumb:hover": { background: "#bfc2cf" },
+            }}
+          >
+            <ItemCard />
+          </Box>
+          {/* Box column footer*/}
+          <Box
+            sx={{
+              height: COLUMN_FOOTER_HEIGHT,
+              p: 2,
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <Button startIcon={<AddCardIcon />}>Add New Card</Button>
+            <Tooltip title="Drag to move column">
+              <DragHandleIcon />
+            </Tooltip>
+          </Box>
+        </Box>
+        {/* Box column */}
+        <Box
+          sx={{
+            minWidth: "300px",
+            maxWidth: "300px",
+            bgcolor: (theme) =>
+              theme.palette.mode === "dark" ? "#333643" : "#ebecf0",
+            ml: 2,
+            borderRadius: "6px",
+            height: "fit-cotent",
+            maxHeight: (theme) =>
+              `calc(${theme.trello.boardContentHeight} - ${theme.spacing(5)})`,
+          }}
+        >
+          {/* Box column header*/}
+          <Box
+            sx={{
+              height: COLUMN_HEADER_HEIGHT,
+              p: 2,
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Typography
+              variant="h6"
+              sx={{ fontWeight: "bold", cursor: "pointer", fontSize: "1rem" }}
+            >
+              Column Tilte
+            </Typography>
+            <LongMenu />
+          </Box>
+          {/* Box list card*/}
+          <Box
+            sx={{
+              p: "0 5px",
+              m: "0 5px",
+              display: "flex",
+              flexDirection: "column",
+              // p: 2,
+              gap: 2,
+              overflowX: "hine",
+              overflowY: "auto",
+              maxHeight: (theme) => `calc(${theme.trello.boardContentHeight} -
+            ${theme.spacing(5)} -
+            ${COLUMN_HEADER_HEIGHT} -
+            ${COLUMN_FOOTER_HEIGHT}
+            )`,
+              "&::-webkit-scrollbar-thumb": {
+                background: "#ced0da",
+                borderRadius: "8px",
+              },
+              "&::-webkit-scrollbar-thumb:hover": { background: "#bfc2cf" },
+            }}
+          >
+            <ItemCard />
+          </Box>
+          {/* Box column footer*/}
+          <Box
+            sx={{
+              height: COLUMN_FOOTER_HEIGHT,
+              p: 2,
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <Button startIcon={<AddCardIcon />}>Add New Card</Button>
+            <Tooltip title="Drag to move column">
+              <DragHandleIcon />
+            </Tooltip>
+          </Box>
+        </Box>
+      </Box>
     </Box>
   )
 }
